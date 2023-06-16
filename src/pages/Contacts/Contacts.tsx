0c93cbbe93cb3b8fa208058caff0ee86ef046634
Ilya_Kargapolov <ilya.kargapolov02@mail.ru>
2023-06-16 16:25:33 +0600

    Обновлена система комментариев

--- src/pages/Contacts/Contacts.tsx
+++ src/pages/Contacts/Contacts.tsx
@@ -3,16 +3,8 @@ import "./styles/Contacts.sass";
 import { Stack, Card, CardContent, Typography, Link, TextField, Button } from "@mui/material";
 import EmailIcon from '@mui/icons-material/Email';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import FeedbackIcon from '@mui/icons-material/Feedback';
 
 export const Contacts = () => {
-    const [feedbackData, setFeedbackData] = React.useState({
-        name: "",
-        email: "",
-        theme: "",
-        message: ""
-    });
-
     React.useEffect(() => {
         document.title = 'Контакты - IcebergPHP';
     }, []);
@@ -92,76 +84,6 @@ export const Contacts = () => {
                                 href="https://github.com/mrProger/IcebergPHP"
                             >GitHub фреймворка</Link>
                         </Typography>
-                    </Stack>
-                </CardContent>
-            </Card>
-            <Card sx={{ mt: 1.5, boxShadow: "0 0 10px rgba(0,0,0,0.5)", width: 500 }}>
-                <CardContent>
-                    <Stack
-                        alignItems="center"
-                        justifyContent="center"
-                        direction="column"
-                    >
-                        <Typography variant="h5" sx={{ fontWeight: "bold" }}>
-                            <Stack
-                                alignItems="center"
-                                justifyContent="center"
-                                direction="row"
-                            >
-                                <FeedbackIcon />
-                                Обратная связь
-                                <FeedbackIcon />
-                            </Stack>
-                        </Typography>
-                        <TextField
-                            label="Имя"
-                            sx={{ mt: 1, width: 450 }}
-                            value={feedbackData.name}
-                            onChange={(event: any) => setFeedbackData({...feedbackData, name: event.target.value})}
-                        />
-                        <TextField
-                            label="Email"
-                            sx={{ mt: 1, width: 450 }}
-                            value={feedbackData.email}
-                            onChange={(event: any) => setFeedbackData({...feedbackData, email: event.target.value})}
-                        />
-                        <TextField
-                            label="Тема"
-                            sx={{ mt: 1, width: 450 }}
-                            value={feedbackData.theme}
-                            onChange={(event: any) => setFeedbackData({...feedbackData, theme: event.target.value})}
-                        />
-                        <TextField
-                            label="Сообщение"
-                            multiline
-                            rows="5"
-                            sx={{ mt: 1, width: 450 }}
-                            value={feedbackData.message}
-                            onChange={(event: any) => setFeedbackData({...feedbackData, message: event.target.value})}
-                        />
-                        <Button
-                            sx={{ mt: 1.5, mb: -0.8 }}
-                            variant="contained"
-                            onClick={() => fetch("http://127.0.0.1:8000/api/v1/addFeedback", {
-                                method: "POST",
-                                headers: {
-                                    "Content-Type": "application/json"
-                                },
-                                body: JSON.stringify({
-                                    name: feedbackData.name,
-                                    email: feedbackData.email,
-                                    theme: feedbackData.theme,
-                                    message: feedbackData.message
-                                })
-                            }).then((response) => {
-                                return response.json().then((resp) => {
-                                    alert(resp.response);
-                                    if (resp.response === "Обращение успешно отправлено") {
-                                        window.location.reload();
-                                    }
-                                });
-                            })}
-                        >Отправить</Button>
                     </Stack>
                 </CardContent>
             </Card>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,33 @@
 import React from "react";
-import { Stack, Card, CardContent, Typography, Link, Button } from "@mui/material";
+import { Stack, Card, CardContent, Typography, Link, Button, TextField } from "@mui/material";
 import "./styles/Home.sass";
+import FeedbackIcon from '@mui/icons-material/Feedback';
 
 export const Home = () => {
+    const [commentData, setCommentData] = React.useState({
+        name: "",
+        message: ""
+    });
+
+    const [comments, setComments] = React.useState<object[]>([])
+
     React.useEffect(() => {
         document.title = 'Главная - IcebergPHP';
+
+        fetch("http://127.0.0.1:8001/api/v1/getComments", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then((response) => {
+            return response.json().then((resp) => {
+                let arr: object[] = [];
+                Object.keys(resp).map((key: any) => {
+                    arr.push(resp[key]);
+                });
+                setComments(arr);
+            });
+        });
     }, []);
 
     return (
@@ -60,6 +83,102 @@ export const Home = () => {
                         Предложить идею
                     </Button>
                 </Link>
+            </Stack>
+            <Stack
+                justifyContent="center"
+                alignItems="center"
+                sx={{ mt: 5 }}
+            >
+                <Card sx={{ mt: 1.5, boxShadow: "0 0 10px rgba(0,0,0,0.5)", width: 500 }}>
+                    <CardContent>
+                        <Stack
+                            alignItems="center"
+                            justifyContent="center"
+                            direction="column"
+                        >
+                            <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+                                <Stack
+                                    alignItems="center"
+                                    justifyContent="center"
+                                    direction="row"
+                                >
+                                    <FeedbackIcon />
+                                    Комментарий
+                                    <FeedbackIcon />
+                                </Stack>
+                            </Typography>
+                            <TextField
+                                label="Имя"
+                                sx={{ mt: 1, width: 450 }}
+                                value={commentData.name}
+                                onChange={(event: any) => setCommentData({ ...commentData, name: event.target.value })}
+                            />
+                            <TextField
+                                label="Сообщение"
+                                multiline
+                                rows="5"
+                                sx={{ mt: 1, width: 450 }}
+                                value={commentData.message}
+                                onChange={(event: any) => setCommentData({ ...commentData, message: event.target.value })}
+                            />
+                            <Button
+                                sx={{ mt: 1.5, mb: -0.8 }}
+                                variant="contained"
+                                onClick={() => commentData.name.trim() !== "" && commentData.message.trim() !== "" ? fetch("http://127.0.0.1:8001/api/v1/addComment", {
+                                    method: "POST",
+                                    headers: {
+                                        "Content-Type": "application/json"
+                                    },
+                                    body: JSON.stringify({
+                                        name: commentData.name,
+                                        message: commentData.message
+                                    })
+                                }).then((response) => {
+                                    return response.json().then((resp) => {
+                                        alert(resp.response);
+                                        if (resp.response === "Комментарий успешно отправлен") {
+                                            let arr = comments;
+                                            arr.push({ name: commentData.name, message: commentData.message });
+                                            setCommentData({ ...commentData, name: "", message: "" });
+                                            setComments(arr);
+                                        }
+                                    });
+                                }) : alert("Обнаружены пустые поля")}
+                            >Отправить</Button>
+                        </Stack>
+                    </CardContent>
+                </Card>
+            </Stack>
+            <Stack
+                justifyContent="center"
+                alignItems="center"
+                sx={{ mt: 5 }}
+            >
+                <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+                    Комментарии
+                </Typography>
+            </Stack>
+            <Stack 
+                justifyContent="center"
+                alignItems="center"
+                sx={{ mt: 2 }}
+            >
+                {
+                    comments && comments.map((comment: any) => (
+                        <Card sx={{ mt: 1.5, boxShadow: "0 0 10px rgba(0,0,0,0.5)", width: "80%" }}>
+                            <Stack>
+                                <CardContent>
+                                    <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+                                        Имя: {comment.name}
+                                    </Typography>
+                                    <Typography sx={{ fontSize: 18, mt: 1, mb: -0.5 }}>
+                                        Комментарий: {comment.message}
+                                    </Typography>
+                                </CardContent>
+                            </Stack>
+                        </Card>
+                    ))
+                }
             </Stack>
         </Stack>
     );
